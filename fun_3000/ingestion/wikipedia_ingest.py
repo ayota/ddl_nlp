94c0cf678f27ce5f696b810926df56e9b9d233a9
alamine42 <alamine@gmail.com>
2015-12-06 15:48:30 -0500

    Ingestion script for Wikipedia

---
+++ fun_3000/ingestion/wikipedia_ingest.py
@@ -0,0 +1,48 @@
+from wikipedia import page as wpg
+from os import path, pardir, makedirs
+
+import logging
+import optparse
+import codecs
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s : %(message)s', level=logging.INFO)
+
+def save_wiki_text(wiki_search_term, storage_path):
+    page = wpg(wiki_search_term)
+    
+    logging.info('Saving data to: %s' % storage_path)
+    with codecs.open(storage_path, 'w+', 'utf-8') as f_out:
+        f_out.write(page.content)
+
+def main():
+
+    parser = optparse.OptionParser()
+    parser.add_option('-s', '--search_term', dest='search_term', default=None, type='string', help='Specify the wikipedia search term (Default is "Disease")')
+    parser.add_option('-d', '--data_directory', dest='data_directory', default=None, help='Specify a directory name for saving search data')
+    (opts, args) = parser.parse_args()
+
+    search_term = opts.search_term
+    data_directory = opts.data_directory
+
+    current_dir = path.dirname(path.realpath(__file__))
+    parent_dir = path.abspath(path.join(current_dir, pardir))
+    root_dir = path.abspath(path.join(parent_dir, pardir))
+
+    data_dir = path.join(root_dir, 'data')
+    if data_directory is not None:
+        model_data_dir = path.join(data_dir, data_directory)
+    else:
+        model_data_dir = path.join(data_dir, search_term)
+
+    if not path.exists(model_data_dir):
+        makedirs(model_data_dir)
+
+    if search_term is not None:
+        logging.info('Retrieving "%s" page from Wikipedia.' % (search_term))
+        local_file_path = model_data_dir + '/model_data.txt'
+        save_wiki_text(search_term, local_file_path)
+    else:
+        logging.info('You have not specified a search term!')
+
+if __name__ == '__main__':
+    main()
