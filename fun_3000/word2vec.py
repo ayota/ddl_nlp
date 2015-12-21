6a583d8290a0dc1474c82f0214ae50e436c1b6be
alamine42 <alamine@gmail.com>
2015-12-20 19:11:44 -0500

    New word2vec module

--- fun_3000/word2vec.py
+++ fun_3000/word2vec.py
@@ -40,10 +40,10 @@ def run_model(input_data_dir, parallel_workers=4, model_name=None):
     parent_dir = path.abspath(path.join(current_dir, pardir))
     
     data_dir = path.join(parent_dir, 'data')
-    model_data_dir = path.join(data_dir, cached_corpus_dir)
+    model_data_dir = path.join(data_dir, input_data_dir)
 
     models_dir = path.join(parent_dir, 'models')
-    this_model_dir = path.join(models_dir, cached_corpus_dir)
+    this_model_dir = path.join(models_dir, input_data_dir)
     if not path.exists(this_model_dir):
         makedirs(this_model_dir)
   
@@ -51,7 +51,7 @@ def run_model(input_data_dir, parallel_workers=4, model_name=None):
     model = train_model(corpus, parallel_workers)
 
     if model_name is None:    
-        model_path = this_model_dir + '/' + cached_corpus_dir +'.model'
+        model_path = this_model_dir + '/' + input_data_dir +'.model'
     else:
         model_path = this_model_dir + '/' + model_name + '.model'
 
@@ -60,13 +60,13 @@ def run_model(input_data_dir, parallel_workers=4, model_name=None):
 if __name__ == '__main__':
 
     parser = optparse.OptionParser()
-    parser.add_option('-i', '--input_data_dir', dest='cached_corpus_dir', default='generic', help='Specify local corpus directory')
+    parser.add_option('-i', '--input_data_dir', dest='input_data_dir', default='generic', help='Specify local corpus directory')
     parser.add_option('-p', '--parallel_workers', dest='parallel_workers', default=1, help='Specify the number of parallel threads', type='int')
     parser.add_option('-o', '--output_model_name', dest='model_name', default=None, help='Specify a name for your model. If not specified, the data directory name will be used.')
     (opts, args) = parser.parse_args()
 
     if opts.model_name is not None:
-        run_model(opts.cached_corpus_dir, opts.parallel_workers, opts.model_name)    
+        run_model(opts.input_data_dir, opts.parallel_workers, opts.model_name)    
     else:
-        run_model(opts.cached_corpus_dir, opts.parallel_workers)
+        run_model(opts.input_data_dir, opts.parallel_workers)
  
