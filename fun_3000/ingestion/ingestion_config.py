53ffb77c25a941c92ff22e69f8dbbce6092afb54
Mehdi El-Amine <mehdi.elamine@circleback.com>
2016-02-04 18:55:49 -0500

    #11 Config file for specifying different online medical db URLS

---
+++ fun_3000/ingestion/ingestion_config.py
@@ -0,0 +1,3 @@
+[medical_abstracts]
+pubmed_search_url=http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=<SEARCH_TERM>&retmax=<RESULTS>
+pubmed_doc_url=http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=<DOC_ID>&rettype=abstract&retmode=text
