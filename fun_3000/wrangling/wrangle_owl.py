e12a226cbe14cc3677022ba9f0badf81803ec7c4
lauralorenz <lalorenz6@gmail.com>
2016-03-17 21:02:56 -0400

    Change wrangle_owl.py to take a static number of graphs specified inline, pull them into a temporary rdflib.Graph along with the OGMS ontology, and write out the rdfs:subClassOf relationships found therein each into their own file. Also with fun little progress bar because why not. #1

--- fun_3000/wrangling/wrangle_owl.py
+++ fun_3000/wrangling/wrangle_owl.py
@@ -1,74 +1,50 @@
 import rdflib
-g = rdflib.Graph()
-g.parse(location="http://ogms.googlecode.com/svn/releases/2014-06-20/ontology/ogms.owl"
-, format="application/rdf+xml")
-f = rdflib.Graph()
-# interested to see how many get converted to literals
-literal_s = 0
-literal_p = 0
-literal_o = 0
-remaining_s = 0
-remaining_p = 0
-remaining_o = 0
-# query will grab labels for s, p, o where it can find it
-# using the namespace rdfs:label
-# otherwise it will return the URI
-# recommended reading: http://www.cambridgesemantics.com/semantic-university/sparql-by-example#%281%29|
-for row in g.query("""
-	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-	SELECT ?s ?s_label ?p_label ?o_label ?p ?o
-WHERE
-{
-  ?s ?p ?o .
-  ?s rdfs:label ?s_label .
-Optional {  ?p rdfs:label ?p_label . }
-Optional {  ?o rdfs:label ?o_label . }
-}
-	"""):
-	# construct triples from the info we have
-	# if the SPARQL could not find something it is None
-	triple = []
-	if row.s_label:
-		triple.append(row.s_label)
-		literal_s= literal_s+1
-	if row.p_label:
-		triple.append(row.p_label)
-		literal_p= literal_p+1
-	else:
-		triple.append(row.p)
-		# checking if p was stored as a literal
-		# never happens in this set
-		if type(row.p) == rdflib.term.Literal:
-			literal_p = literal_p+1
-		else:
-			remaining_p=remaining_p+1
-	if row.o_label:
-		triple.append(row.o_label)
-		literal_o= literal_o+1
-	else:
-		triple.append(row.o)
-		# checking if o was stored as a literal
-		# common in this set
-		if type(row.o) == rdflib.term.Literal:
-			literal_o = literal_o+1
-		else:
-			remaining_o=remaining_o+1
-	f.add(triple)
+from copy import copy
+import progressbar
 
-# now for metrics
-print("length of graph: ", len(f))
-# problem URIs appear to be in Turtle
-# and specifically items from core i.e. rdf-schema, 22-rdf-syntax-ns, or owl namespaces
-print("literal s", literal_s)
-print("remaining s", remaining_s)
-print("literal p", literal_p)
-print("remaining p", remaining_p)
-print("literal o", literal_o)
-print("remaining o", remaining_o)
-# ('length of graph: ', 541)
-# ('literal s', 541)
-# ('remaining s', 0)
-# ('literal p', 295)
-# ('remaining p', 246)
-# ('literal o', 414)
-# ('remaining o', 127)
+# various rdf locations
+# let's mix OGMS ontology with instance ontologies
+
+OGMS = "http://ogms.googlecode.com/svn/releases/2014-06-20/ontology/ogms.owl"
+# make a graph we'll read our stuff into
+ogms_graph = rdflib.Graph().parse(location=OGMS, format='application/rdf+xml')
+
+# some instance ontologies
+other_ontologies = {
+    "INFECTIOUS DISEASE": {
+        "output_filename": "infectious_disease.txt",
+        "location": "http://infectious-disease-ontology.googlecode.com/svn/releases/2014-08-01/ido.owl"
+    },
+    "ORAL HEALTH AND DISEASE": {
+        "output_filename": "oral_health_and_disease.txt",
+        "location": "http://purl.obolibrary.org/obo/ohd/dev/ohd.owl"
+    },
+    "BIOMEDICAL INVESTIGATIONS": {
+        "output_filename": "biomedical_investigations.txt",
+        "location": "http://purl.obolibrary.org/obo/obi.owl"
+    }
+}
+
+progress = 0
+with progressbar.ProgressBar(max_value=len(other_ontologies)) as bar:
+    for graph in other_ontologies:
+        progress += 1
+        g = copy(ogms_graph)
+
+        # parse them into our holding graph 'g'
+        g.parse(location=OGMS, format="application/rdf+xml")
+        g.parse(location=other_ontologies[graph]['location'], format="application/rdf+xml")
+
+        # a query to get slabel and olabel where the relationships is rdfs:subClassOf
+        query = """SELECT ?slabel ?label
+                    WHERE
+                        {?s rdfs:subClassOf ?o .
+                        ?s rdfs:label ?slabel .
+                        FILTER (isIRI(?o)).
+                        ?o rdfs:label ?label} """
+
+        with open(other_ontologies[graph]['output_filename'], "w") as writer:
+            for row in g.query(query):
+                # save these triples somewhere
+                writer.write("{s} {p} {o}\n".format(s=row[0], p="is", o=row[1]))
+        bar.update(progress)
