094411ca581db6c1d058254afee2d08a73134d19
lauralorenz <lalorenz6@gmail.com>
2016-03-31 18:51:44 -0400

    Clean up a little bit, add a docstring, gitignore the .txt files generated in wrangling/ for now and will add to the Drakefile to pull them in before word2vec starts. Took out ingest_owl since the steps are no longer separate.

--- fun_3000/ingestion/ingest_owl.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get("http://ogms.googlecode.com/svn/releases/2014-06-20/ontology/ogms.owl")
-with open("ogms.owl", "w") as f: #need a real place to put this
-	f.write(r.text) # is a unicode object
