337e6857684581e9e2900045bf53aded6595d999
lauralorenz <lalorenz6@gmail.com>
2016-12-20 21:14:15 -0500

    Update word2vec to take the single input file and output the single model into the data directory.

--- fun_3000/word2vec.py
+++ fun_3000/word2vec.py
@@ -10,13 +10,12 @@ logging.basicConfig(format='%(asctime)s: %(levelname)s : %(message)s', level=log
 
 class MySentences(object):
 
-    def __init__(self, dirname):
-        self.dirname = dirname
+    def __init__(self, file_path):
+        self.file_path = file_path
 
     def __iter__(self):
-        for input_file_name in listdir(self.dirname):
-            for line in open(path.join(self.dirname, input_file_name)):
-                yield line.split()                                             
+        for line in open(path.join(self.file_path)):
+            yield line.split()
 
 def timeit(method):
 
@@ -30,78 +29,30 @@ def timeit(method):
 
     return timed
 
-def run_model(input_data_dir, parallel_workers=4, hidden_layer=100, context_window=5, model_name=None):
-    """
-    This function runs gensim.models.Word2Vec on all folds for a given input data directory.  Specifically, this wrapper function
-    derives the proper right directories to search for the data-folds and then loops over those locations running ron_model_fold
-    for each.
-    :param input_data_dir: The name of the location where the input data is coming.
-    :param parallel_workers:
-    :param hidden_layer: The number of neurons in the hidden layer of the word2vec model
-    :param context_window: The size of teh context window
-    :param model_name: the name of the model file to be output.  If none then it outputs the input_data_dir as the filename.
-    :return:
-    """
-    # Set data directory
-    current_dir = path.dirname(path.realpath(__file__))
-    parent_dir = path.abspath(path.join(current_dir, pardir))
-
-    data_dir = path.join(parent_dir, 'data')
-    this_data_dir = path.join(data_dir, input_data_dir)
-    models_dir = path.join(parent_dir, 'models')
-    this_model_dir = path.join(models_dir, input_data_dir)
-
-    if not path.exists(this_model_dir):
-        makedirs(this_model_dir)
-
-    #get the folder names under the data directory.  Each directory represents a fold and we need to loop through
-    model_directory_fold_directories = filter(lambda x: path.isdir(path.join(this_data_dir, x)), listdir(this_data_dir))
-
-    #loop through each of the fold subdirectories providing the folder location for the data each time and the intended
-    # location for the resulting model file to be stored.
-    for idx, fold_directory in enumerate(model_directory_fold_directories):
-
-        logging.info('')
-        logging.info('-------------------------')
-        logging.info('Run Word2Vec for fold # %s' % str(idx+1))
-        logging.info('-------------------------')
-        logging.info('')
-
-        model_fold_directory=path.join(this_model_dir, fold_directory)
-        fold_data_directory=path.join(this_data_dir, fold_directory)
-        fold_model_data_dir = path.join(fold_data_directory, 'train')
-        run_model_fold(input_data_dir=input_data_dir,
-                       model_data_dir=fold_model_data_dir,
-                       this_model_dir=model_fold_directory,
-                       parallel_workers=parallel_workers,
-                       hidden_layer=hidden_layer,
-                       context_window=context_window,
-                       model_name=model_name)
-
-def run_model_fold(input_data_dir, model_data_dir, this_model_dir, parallel_workers=4, hidden_layer=100, context_window=5, model_name=None):
+def run_model(data_directory, boosted_filename, parallel_workers=4, hidden_layer=100, context_window=5, model_name=None):
     '''
     Build a word2vec model of the provided corpus.
-    :param input_data_dir:
+    :param data_directory:
     :param parallel_workers:
     :param hidden_layer:
     :param context_window:
     :param model_name:
     :return:
     '''
+    # Set data directory
+    current_dir = path.dirname(path.realpath(__file__))
+    parent_dir = path.abspath(path.join(current_dir, pardir))
 
-    corpus = MySentences(model_data_dir)
+    abs_path_to_data_directory = path.join(parent_dir, 'data', data_directory)
+    input_file_path = path.join(abs_path_to_data_directory, boosted_filename)
+
+    corpus = MySentences(input_file_path)
     model = gensim.models.Word2Vec(corpus, workers=parallel_workers, size=hidden_layer, window=context_window)
 
-    if not path.exists(this_model_dir):
-        makedirs(this_model_dir)
-
-    if model_name is None:
-        # Replace / with _ to prevent creation of unecessary directories, because this expects the fold structure
-        filename = input_data_dir.replace('/', '_')
-        logging.info(filename)
-        model_path = this_model_dir + '/' + filename +'.model'
-    else:
-        model_path = this_model_dir + '/' + model_name + '.model'
+    # Replace / with _ to prevent creation of unecessary directories, because this expects the fold structure
+    filename = data_directory.replace('/', '_')
+    logging.info(filename)
+    model_path = abs_path_to_data_directory + '/' + filename +'.model'
 
     print model_path
     model.save(model_path)
@@ -109,14 +60,13 @@ def run_model_fold(input_data_dir, model_data_dir, this_model_dir, parallel_work
 if __name__ == '__main__':
 
     parser = optparse.OptionParser()
-    parser.add_option('-i', '--input_data_dir', dest='input_data_dir', default='generic', help='Specify local corpus directory')
+    parser.add_option('-d', '--data_directory', dest='data_directory', default='generic', help='Specify local corpus directory')
+    parser.add_option('-f', '--filename', dest='input_filename', default='1_boost_output.txt',
+                      help="Specify the name of the file that contains the boosted corpus.")
     parser.add_option('-p', '--parallel_workers', dest='parallel_workers', default=1, help='Specify the number of parallel threads', type='int')
     parser.add_option('-w', '--window_size', dest='context_window', default=5, help='Specify the context window size', type='int')
     parser.add_option('-l', '--hidden_layer_size', dest='hidden_layer', default=100, help='Specify the hidden layer size', type='int')
-    parser.add_option('-o', '--output_model_name', dest='model_name', default=None, help='Specify a name for your model. If not specified, the data directory name will be used.')
+
     (opts, args) = parser.parse_args()
 
-    if opts.model_name is not None:
-        run_model(opts.input_data_dir, opts.parallel_workers, opts.context_window, opts.hidden_layer, opts.model_name)    
-    else:
-        run_model(opts.input_data_dir, opts.parallel_workers, opts.context_window, opts.hidden_layer)
+    run_model(opts.data_directory, opts.input_filename, opts.parallel_workers, opts.context_window, opts.hidden_layer)
